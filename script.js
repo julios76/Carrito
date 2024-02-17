76a1c8dddd3d7ea45ebdada21673867f382485f2
julios76 <julsosa@gmail.com>
2024-02-17 06:27:42 -0300

    vulnerabilidad

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const products = [
     { id: 1, name: 'Producto 1', price: 10 },
     { id: 2, name: 'Producto 2', price: 20 },
-    { id: 3, name: 'Producto 3', price: 30 }
+    { id: 3, name: 'Producto 3', price: 30 },
+	{ id: 4, name: '<script>alert("¡Vulnerabilidad de XSS!")</script>', price: 0 }
 ];
 
 // Función para mostrar los productos disponibles
